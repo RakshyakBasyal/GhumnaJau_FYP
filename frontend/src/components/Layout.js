648de83e264528de5ab0a6af1b2b771059622129
RakshyakBasyal <np03cs4s240112@heraldcollege.edu.np>
2025-12-31 23:20:52 +0545

    Minor layout/api tweaks

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,1 +1,20 @@
-import { Outlet } from 'react-router-dom'; import Navbar from './Navbar'; import Footer from './Footer'; const Layout = () => { return ( <div className="flex flex-col min-h-screen"> <Navbar /> <main className="flex-1"> <Outlet /> {/* This renders the child page (Landing, Destinations, etc.) */} </main> <Footer /> </div> ); }; export default Layout;
+// import { Outlet } from 'react-router-dom'; import Navbar from './Navbar'; import Footer from './Footer'; const Layout = () => { return ( <div className="flex flex-col min-h-screen"> <Navbar /> <main className="flex-1"> <Outlet /> {/* This renders the child page (Landing, Destinations, etc.) */} </main> <Footer /> </div> ); }; export default Layout;
+
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+        {/* This renders the child page (Landing, Destinations, etc.) */}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
